556d82089a23ee8ce4ec46d5aae4db66c34f0a1e
OctaneXyz01 <adedejiolamide461@gmail.com>
2022-03-14 18:58:36 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ *
+ * Description: print single digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(0);
+
+	return (0);
+}
